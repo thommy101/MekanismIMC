0a0d61c1a2ad7af2f01d4135be585697f08a6dd7
Tom Elfring <tomelfring@gmail.com>
2017-04-16 15:12:11 +0200

    Add removal through IMC

--- src/main/java/eu/elfring/mekanismimc/MekanismIMC.java
+++ src/main/java/eu/elfring/mekanismimc/MekanismIMC.java
@@ -9,7 +9,7 @@ import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLInterModComms;
 
-@Mod(modid = MekanismIMC.MODID, version = MekanismIMC.VERSION)
+@Mod(modid = MekanismIMC.MODID, version = MekanismIMC.VERSION, dependencies = "after:mekanism")
 public class MekanismIMC {
     public static final String MODID = "mekanismimc";
     public static final String VERSION = "1.0";
@@ -20,5 +20,10 @@ public class MekanismIMC {
         recipeTag.setTag("input", new ItemStack(Blocks.COBBLESTONE).writeToNBT(new NBTTagCompound()));
         recipeTag.setTag("output", new ItemStack(Items.DIAMOND).writeToNBT(new NBTTagCompound()));
         FMLInterModComms.sendMessage("Mekanism", "EnrichmentChamberRecipe", recipeTag);
+
+        NBTTagCompound recipeRemovalTag = new NBTTagCompound();
+        recipeRemovalTag.setTag("input", new ItemStack(Blocks.DIAMOND_ORE).writeToNBT(new NBTTagCompound()));
+        recipeRemovalTag.setTag("output", new ItemStack(Items.DIAMOND, 2).writeToNBT(new NBTTagCompound()));
+        FMLInterModComms.sendMessage("Mekanism", "RemoveEnrichmentChamberRecipe", recipeRemovalTag);
     }
 }
